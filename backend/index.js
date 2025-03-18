14bd52f883c02cbe9e328e098dfd3309a52abd29
VishalShrimali <24vishalmali@gmail.com>
2025-03-18 14:43:36 +0530

    Whatsapp done

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 import express, { urlencoded } from "express";
 import { connectionDB } from "./src/utils/database.utils.js";
 import dotenv from "dotenv";
+import cron from "node-cron";
 import { adminRouter } from "./src/routes/admin.routes.js";
+import { employeeRouter } from "./src/routes/employee.routes.js";
+import { sendGreetings } from "./src/utils/Greetings.utils.js";
+import moment from 'moment-timezone';
 dotenv.config();
 
 const app = express();
@@ -12,10 +16,25 @@ app.use(express.urlencoded({extended: true}));
 
 
 app.use('/api/v1/admin', adminRouter)
+app.use('/api/v1/employee', employeeRouter)
+
+cron.schedule("* * * * *", async () => {
+    console.log("✅ Cron Job Triggered at:", new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" }));
+  
+    try {
+      await sendGreetings();
+      console.log("✅ Greetings job executed successfully.");
+    } catch (error) {
+      console.error("❌ Error in cron job:", error.message);
+    }
+  }, {
+    scheduled: true,
+    timezone: "Asia/Kolkata"
+  });
 
 const port = process.env.PORT || 5100
 
 app.listen(port, (req, res) => {
     console.log(`Server is running on ${port}`);
-    connectionDB();
+    
 })
