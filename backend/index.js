e9a61fbb8b3b36e614aca636caf5f68d1b68d6e5
devp317 <devpatel31007@gmail.com>
2025-05-04 19:47:51 +0530

    Update CORS configuration to allow live site also

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+const allowedOrigins = ['http://localhost:5173', 'https://hrms-n8v4.onrender.com'];
+app.use(cors({ origin: allowedOrigins ,credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
