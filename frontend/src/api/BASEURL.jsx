e8a6486da2ee31a8322a7a1048f4474d8351dabc
devp317 <devpatel31007@gmail.com>
2025-05-04 20:05:45 +0530

    change base url in frontend

--- frontend/src/api/BASEURL.jsx
+++ frontend/src/api/BASEURL.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8000/api/v1";
+export const API_BASE_URL = "/api/v1";
 
 export const API = () => {
   const token = localStorage.getItem("token");
